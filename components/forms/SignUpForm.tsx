679d4429517c4cc12a4a7989a3b4b3be2cf0b2fb
YUapisit <aps.apisit250@gmail.com>
2024-06-25 15:51:47 +0700

    console

--- components/forms/SignUpForm.tsx
+++ components/forms/SignUpForm.tsx
@@ -28,7 +28,7 @@ function SignUpForm() {
         }
       })
       .catch((err) => {
-        console.log(err)
+        
         if (err.response.status == 400) {
           Swal.fire({
             title: "Error!",
